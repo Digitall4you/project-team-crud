1bbf21b722c1e929ebad59aacb47cd82784db417
Alindrianja <amiseru@gmail.com>
2018-07-26 12:04:12 +0300

    selection des membres dans la base

--- index.mjs
+++ index.mjs
@@ -1,13 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 import Member from './Member.model';
 
 mongoose.connect('mongodb://localhost/');
 
 mongoose.connection.on('connected', () => {
-    console.log('connecté');
+    console.log('connection to database established');
 
     const app = express();
     app.use(bodyParser.json());
@@ -18,26 +18,31 @@ mongoose.connection.on('connected', () => {
     });
 
     app.get('/member', (req, res) => {
+        //recuperation de tous les membres
         Member.find().then((members) => {
             res.json(members);
         });
-    
     });
 
+    app.get('/member/:id', (req, res) => {
+        //Sélectionne un membre
+        console.log('Get member by id, params:', req.params.id);
+        Member.find({_id: req.params.id}).then(member => {
+            res.json(member);
+        });
+    });
+    
     app.post('/member', (req, res) => {
         console.log('POST', req.body);
-    /*    const member = {
-            nom: 'Rasoa',
-            prenom: 'Herina',
-            role: 'programmeur'
-    };
+        //Ajouter de membre dans la bd
+        const member = req.body;
         Member.create(member).then(() => {
-            console.log("membre ajouté");
-        });*/
+            console.log('membre ajouté');
+        });
     });
 });
 
 mongoose.connection.on('error', () => {
-    throw new Error('connexion impossible');
+    throw new Error('unable to establish connection');
 }); 
 
